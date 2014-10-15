52173f5d0bab6c3879e65eabcae28662cb891081
Hugo van den Berg <hugo@tbdwebdesign.nl>
2014-10-15 10:04:19 +0200

    modified:   src/project_point_on_line.m         - Added to_endpoint option to convert project points beyond line to closest endpoint         - Modified to use to_endpoint option

--- src/project_point_on_line.m
+++ src/project_point_on_line.m
@@ -1,9 +1,12 @@
-function [projections, distance] = project_point_on_line(points, line_mat, as_fraction)
+function [projections, distance] = project_point_on_line(points, line_mat, as_fraction, to_endpoint)
     if size(line_mat, 1) ~= 2 || size(line_mat, 2) ~= size(points, 2)
         error('Line ill defined')
     end
-    if nargin ~= 3
+    if nargin < 3
         as_fraction = false;
+    end
+    if nargin < 4
+        to_endpoint = false;
     end
         
     vec_0 = line_mat(1, :);
@@ -11,6 +14,9 @@ function [projections, distance] = project_point_on_line(points, line_mat, as_fr
     for i = size(points, 1):-1:1
         vec_H = points(i, :) - vec_0;
         rho = sum(vec_H.*vec_L)/sum(vec_L.*vec_L);
+        if to_endpoint
+            rho = min(1, max(0, rho));
+        end
         if as_fraction
             projections(i) = rho;
             if nargout > 1
