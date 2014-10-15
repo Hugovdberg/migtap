0de612d5f85a58cd92d377f15276651a41928a98
Hugo van den Berg <hugo@tbdwebdesign.nl>
2014-10-15 10:04:19 +0200

    modified:   src/project_point_on_line.m         - Added to_endpoint option to convert project points beyond line to closest endpoint         - Modified to use to_endpoint option

--- src/project_point_on_multiline.m
+++ src/project_point_on_multiline.m
@@ -5,12 +5,14 @@ function [projections, distances, line_index] = project_point_on_multiline(point
     for j = size(points, 1):-1:1
         % first determine projection on each linepart as fraction
         for i = size(line_mat, 1)-1:-1:1
-            project_frac(i) = project_point_on_line(points(j, :), ...
-                line_mat(i:i+1, :), true);
-        end
-	ind = find(project_frac >= 0.0 & project_frac <= 1.0);
-        for i = length(ind):-1:1
-            [line_proj(i, :), line_dist(i, 1)] = project_point_on_line(points(j, :), line_mat(ind(i):ind(i)+1, :));
+%            project_frac(i) = project_point_on_line(points(j, :), ...
+%                line_mat(i:i+1, :), true);
+%        end
+%	ind = find(project_frac >= 0.0 & project_frac <= 1.0);
+%        for i = length(ind):-1:1
+            as_fraction = false;
+            to_endpoint = true;
+            [line_proj(i, :), line_dist(i, 1)] = project_point_on_line(points(j, :), line_mat(ind(i):ind(i)+1, :), as_fraction, to_endpoint);
         end
         [min_dist, min_indx] = min(line_dist);
         projections(j, :) = line_proj(min_indx, :);
