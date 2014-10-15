2dd43fa8b2b2a51b877aae4a8301eb4b2e873ac8
Hugo van den Berg <hugo@tbdwebdesign.nl>
2014-10-15 10:16:54 +0200

    Fixed index and comments

--- src/project_point_on_multiline.m
+++ src/project_point_on_multiline.m
@@ -2,18 +2,14 @@ function [projections, distances, line_index] = project_point_on_multiline(point
     if size(line_mat, 2) ~= size(points, 2) || size(line_mat, 1) < 2
         error('Lines ill defined')
     end
+    as_fraction = false;
+    to_endpoint = true;
     for j = size(points, 1):-1:1
-        % first determine projection on each linepart as fraction
+        % Determine projection on each line part, set to endpoint of linepart if beyond line
         for i = size(line_mat, 1)-1:-1:1
-%            project_frac(i) = project_point_on_line(points(j, :), ...
-%                line_mat(i:i+1, :), true);
-%        end
-%	ind = find(project_frac >= 0.0 & project_frac <= 1.0);
-%        for i = length(ind):-1:1
-            as_fraction = false;
-            to_endpoint = true;
-            [line_proj(i, :), line_dist(i, 1)] = project_point_on_line(points(j, :), line_mat(ind(i):ind(i)+1, :), as_fraction, to_endpoint);
+            [line_proj(i, :), line_dist(i, 1)] = project_point_on_line(points(j, :), line_mat(i:i+1, :), as_fraction, to_endpoint);
         end
+        % Find shortest distance to linepart, including index of that part
         [min_dist, min_indx] = min(line_dist);
         projections(j, :) = line_proj(min_indx, :);
         if nargout > 1
