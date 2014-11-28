2cde15ce647c7072faf350204f0c5aac2e44befe
Hugo van den Berg <hugo@tbdwebdesign.nl>
2014-11-28 08:43:18 +0100

    Added plot_project_point_on_line.m
    Added function to plot points, projected points, and connecting lines

---
+++ src/plot_project_point_on_line.m
@@ -0,0 +1,39 @@
+% project_point_on_line, part of the Migtap library
+%    Copyright (C) 2014 Hugo van den Berg
+%
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU Lesser General Public License as 
+%    published by the Free Software Foundation, either version 3 of the 
+%    License, or (at your option) any later version.
+%
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU Lesser General Public License
+%    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+function [h, projections, distance, projections_relative] = plot_projection_point_on_line(points, line_mat, to_endpoint, fig)
+    if size(line_mat, 2) ~= 2
+        error('plot_projection_point_on_line supports only two dimensional lines')
+    end
+    if nargin < 3
+    	to_endpoint = false;
+    end
+    [projections, distance, projections_relative] = project_point_on_line(points, line_mat, to_endpoint);
+    
+    if nargin < 4
+        fig = figure();
+    else
+        set(0, 'currentfigure', fig);
+    end
+    hold on;
+    h = NaN(size(points, 1), 2);
+    h(size(points, 1)+1, 1) = plot(line_mat(:, 1), line_mat(:, 2));
+    for i=1:size(points, 1)
+        h(i, 1) = plot([points(i, 1), projections(i, 1)], [points(i, 2), projections(i, 2)], '--');
+        h(i, 2) = plot([points(i, 1), projections(i, 1)], [points(i, 2), projections(i, 2)], '*');
+	set(h(i, 1), 'Color', [0.2, 0.2, 0.2]);
+	set(h(i, 2), 'Color', [0.1, 0.1, 0.1]);
+    end
+end
