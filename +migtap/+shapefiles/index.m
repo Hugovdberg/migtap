4ded710749ff7f4360ace896e089cdbfa7a32a6c
Hugo van den Berg <hugo@tbdwebdesign.nl>
2016-01-25 00:01:55 +0100

    moved lots of constants to static class variables for clarity

--- +migtap/+shapefiles/index.m
+++ +migtap/+shapefiles/index.m
@@ -1,6 +1,8 @@
 function shpIndex = index(filename)
 %READ Reads ESRI shapefile and returns structarray with shapes and records
 
+    sc = migtap.shapefiles.mixin.ShapeConsts;
+
     READ_BINARY = 'rb';
     BIG_ENDIAN = 'ieee-be';
     LITTLE_ENDIAN = 'ieee-le';
@@ -19,19 +21,30 @@ function shpIndex = index(filename)
     RECORD_LENGTH_OFFSET = 4;
     RECORD_LENGTH_SIZE = 4;
 
-    shpInfo = migtap.shapefiles.info(filename);
+    standalone = ischar(filename);
+    if standalone
+        fid = fopen(filename, sc.READ_BINARY);
+    else
+        fid = filename;
+        filename = fopen(fid);
+    end
+
+    shpInfo = migtap.shapefiles.info(fid);
     numrecords = (shpInfo.FileLength-50)/4;
 
-    fid = fopen(filename, READ_BINARY, BIG_ENDIAN);
 
-    fseek(fid, HEADER_LENGTH+RECORD_OFFSETS_OFFSET, BEGIN_OF_FILE);
-    offsets = fread(fid, [numrecords, 1], INTEGER, RECORD_OFFSETS_SIZE);
-    fseek(fid, HEADER_LENGTH+RECORD_LENGTH_OFFSET, BEGIN_OF_FILE);
-    lengths = fread(fid, [numrecords, 1], INTEGER, RECORD_LENGTH_SIZE);
+    fseek(fid, sc.FILE_HEADER_LENGTH+sc.INDEX_OFFSETS_OFFSET, sc.BEGIN_OF_FILE);
+    offsets = fread(fid, [numrecords, 1], sc.INTEGER, sc.INDEX_OFFSETS_SIZE, sc.BIG_ENDIAN);
+    fseek(fid, sc.FILE_HEADER_LENGTH+sc.INDEX_LENGTH_OFFSET, sc.BEGIN_OF_FILE);
+    lengths = fread(fid, [numrecords, 1], sc.INTEGER, sc.INDEX_LENGTH_SIZE, sc.BIG_ENDIAN);
 
     shpIndex.FileName = filename;
     shpIndex.NumRecords = numrecords;
     shpIndex.Offsets = offsets;
     shpIndex.Lengths = lengths;
+    
+    if standalone
+        fclose(fid);
+    end
 end
 
--- +migtap/+shapefiles/read.m
+++ +migtap/+shapefiles/read.m
@@ -1,22 +1,59 @@
-function shapes = read(filename, selection)
+function shapes = read(filename, selection, attributes)
 %READ Reads ESRI shapefile and returns structarray with shapes and records
 
     % Declare constants
-    RECORD_LENGTH_OFFSET = 0;
-    RECORD_CONTENT_LENGTH_OFFSET = 4;
+    sc = migtap.shapefiles.mixin.ShapeConsts();
 
     [path, fname, ~] = fileparts(filename);
     prefix = fullfile(path, fname);
-    shpfile = [prefix '.shp'];
-    shxfile = [prefix '.shx'];
-    dbffile = [prefix '.dbf'];
+    shpfile = fopen([prefix '.shp'], sc.READ_BINARY);
+    shxfile = fopen([prefix '.shx'], sc.READ_BINARY);
+    dbffile = fopen([prefix '.dbf'], sc.READ_BINARY);
+
+    if nargin < 3
+        attributes = [];
+    end
+    try
+        shpInfo = migtap.shapefiles.info(shpfile);
+        shxInfo = migtap.shapefiles.index(shxfile);
+        shpInfo.NumRecords = shxInfo.NumRecords;
+
+        if nargin > 1 && ~isempty(selection)
+            assert(...
+                all(selection <= shxInfo.NumRecords), ...
+                'SHPREAD:invalidRecordNumber', ...
+                'Record# %d does not exist. (#records: %d)', ...
+                max(selection), shxInfo.NumRecords ...
+                )
+        else
+            selection = 1:shxInfo.NumRecords;
+        end
+
+        shpData = shpInfo.ShapeTypeParser(shpfile, shxInfo, selection);
+        [dbfData, dbfInfo] = dbf.read(dbffile, selection, attributes);
+
+        shapes.Shape = shpData;
+        shapes.Record = cell2struct(dbfData, {dbfInfo.FieldInfo.Name}, 2);
+        shapes.Meta.Shape = shpInfo;
+        shapes.Meta.Index = shxInfo;
+        shapes.Meta.Record = dbfInfo;
+    catch err
+        try
+            fclose(dbffile);
+        catch
+        end
+        try
+            fclose(shxfile);
+        catch
+        end
+        try
+            fclose(shpfile);
+        catch
+        end
+        rethrow(err)
+    end
     
-    shpInfo = migtap.shapefiles.info(shpfile);
-    shxInfo = migtap.shapefiles.index(shxfile);
-    dbfInfo = dbfread(dbffile);
-
-    shapes.shp = shpInfo;
-    shapes.idx = shxInfo;
-    shapes.rec = dbfInfo;
+    fclose(dbffile);
+    fclose(shxfile);
+    fclose(shpfile);
 end
-
